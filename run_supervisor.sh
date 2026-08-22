0f3986a13f47f107f50607639a62b64ed8064119
SHOGUN <you@termux.empire>
2026-08-22 03:46:34 -0400

    Make supervisor backup step conditional

--- run_supervisor.sh
+++ run_supervisor.sh
@@ -3,9 +3,9 @@ set -e
 cd ~/BIRTH_EDGE
 
 mkdir -p backups
-cp data/birth_edge.db backups/birth_edge_$(date +%Y%m%d_%H%M%S).db
-cp data/learning.db   backups/learning_$(date +%Y%m%d_%H%M%S).db
-cp data/cognition.db  backups/cognition_$(date +%Y%m%d_%H%M%S).db
+[ -f data/birth_edge.db ] && cp data/birth_edge.db backups/birth_edge_$(date +%Y%m%d_%H%M%S).db
+[ -f data/learning.db ]   && cp data/learning.db   backups/learning_$(date +%Y%m%d_%H%M%S).db
+[ -f data/cognition.db ]  && cp data/cognition.db  backups/cognition_$(date +%Y%m%d_%H%M%S).db
 
 ls -1t backups/birth_edge_*.db  | tail -n +21 | xargs -r rm -f
 ls -1t backups/learning_*.db    | tail -n +21 | xargs -r rm -f
